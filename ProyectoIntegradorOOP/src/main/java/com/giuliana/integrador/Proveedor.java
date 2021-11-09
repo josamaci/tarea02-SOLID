7a58ece2f5aaf63abd1b7bb3c8af809d75f76ab6
nicolas <niplinig@espol.edu.ec>
2021-11-09 18:25:41 -0500

    Eliminando el Proyecto Integrador para volverlo a cargar

--- ProyectoIntegradorOOP/src/main/java/com/giuliana/integrador/Proveedor.java
+++
@@ -1,9 +0,0 @@
-package com.giuliana.integrador;
-
-public class Proveedor {
-
-    void proveerMateriaPrima(int paquetes){
-        System.out.println("Se ha provisto a la empresa de " + paquetes + " paquetes de materia prima.");       
-    }
-    
-}
